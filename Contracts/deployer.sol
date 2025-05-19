b2db565eeac699dc3766281df7beef6daa793ad9
Mayuresh Pawar <100525765+mayureshpwr@users.noreply.github.com>
2025-05-19 20:24:01 +0530

    Update deployer.sol

--- Contracts/deployer.sol
+++ Contracts/deployer.sol
@@ -9,7 +9,7 @@ ______           _                         _____             _                  
           |_|            |___/                                                      
 **/
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
